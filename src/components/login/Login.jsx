5cf6f71c2c5ecb7867ba935b08da8422f416d985
Bharateshwar Singh <bharateshwarsp@gmail.com>
2020-09-02 19:07:30 +0530

    Validations added

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,26 +1,36 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Card, Form, Button, Alert } from 'react-bootstrap'
+import { useForm } from 'react-hook-form'
 import './Login.css'
 
 const Login = props => {
 
-    const [loginButtonEnabled] = useState(false)
+    const { register, handleSubmit, errors, formState } = useForm()
+
+    const { touched } = formState;
+    const onSubmit = data => console.log(data)
+
+
     return (
         <div className="login-component align-items-center d-flex">
             <Card id="login-card">
                 <Card.Body>
                     <Card.Title className="display-4">Loan Management App</Card.Title>
-                    <form className="login-form">
+                    <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
                         <Form.Group controlId="formEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" size="lg" />
+                            <Form.Control name="email" placeholder="Enter email" size="lg"
+                                ref={register({ required: true, pattern: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/ })} />
                         </Form.Group>
                         <Form.Group controlId="formPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Enter password" size="lg" />
+                            <Form.Control name="password" type="password" placeholder="Enter password" size="lg"
+                                ref={register({ required: true, min: 1 })} />
                         </Form.Group>
-                        <Alert variant="danger">Incorrect email or password!</Alert>
-                        <Button type="submit" className="w-100" disabled={!loginButtonEnabled}>Login</Button>
+                        {(errors.email || errors.password) && <Alert variant="danger">Invalid email/password!</Alert>}
+                        <Button type="submit" className="w-100" disabled={(Object.keys(touched).length !== 2) ||
+                            (Object.keys(errors).length > 0)}>Login</Button>
+
                     </form>
                 </Card.Body>
             </Card>
