cea550f718e8134bedbede5aeada28ced95cd4b0
Bharateshwar Singh <bharateshwarsp@gmail.com>
2020-09-03 19:54:23 +0530

    test IDs added

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -12,23 +12,23 @@ const Login = props => {
 
 
     return (
-        <div className="login-component align-items-center d-flex">
+        <div data-testid = "login" className="login-component align-items-center d-flex">
             <Card id="login-card">
                 <Card.Body>
                     <Card.Title className="display-4">Loan Management App</Card.Title>
                     <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
                         <Form.Group controlId="formEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control name="email" placeholder="Enter email" size="lg"
+                            <Form.Control data-testid="email-input" name="email" placeholder="Enter email" size="lg"
                                 ref={register({ required: true, pattern: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/ })} />
                         </Form.Group>
                         <Form.Group controlId="formPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control name="password" type="password" placeholder="Enter password" size="lg"
+                            <Form.Control data-testid="password-input" name="password" type="password" placeholder="Enter password" size="lg"
                                 ref={register({ required: true, min: 1 })} />
                         </Form.Group>
                         {(errors.email || errors.password) && <Alert variant="danger">Invalid email/password!</Alert>}
-                        <Button type="submit" className="w-100" disabled={(Object.keys(touched).length !== 2) ||
+                        <Button data-testid="login-button" type="submit" className="w-100" disabled={(Object.keys(touched).length !== 2) ||
                             (Object.keys(errors).length > 0)}>Login</Button>
 
                     </form>
