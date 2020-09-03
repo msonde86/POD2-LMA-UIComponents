e35ce4e2e7b31ddf89421e125487246eb186ce15
Bharateshwar Singh <bharateshwarsp@gmail.com>
2020-09-03 19:54:40 +0530

    Tests Added/Removed

---
+++ src/components/login/__test__/Login.test.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import Login from '../Login'
+import 'mutationobserver-shim';
+import {render} from '@testing-library/react'
+import "@testing-library/jest-dom/extend-expect"
+
+test("renders without crashing",()=>{
+    const div = document.createElement("div")
+    ReactDom.render(<Login/>,div)
+    ReactDom.unmountComponentAtNode(div)
+})
+
+test("contains required text content only",()=>{
+   const {getByTestId} = render(<Login/>)
+   expect(getByTestId('login')).toHaveTextContent("Loan Management AppEmail addressPasswordLogin")
+})
+
+test("login button is disabled on load",()=>{
+    const {getByTestId} = render(<Login/>)
+    expect(getByTestId('login-button')).toBeDisabled()
+})
+
+test("email input exists",()=>{
+    const {getByTestId} = render(<Login/>)
+    expect(getByTestId('email-input')).toBeTruthy()
+})
+
+test("password input exists",()=>{
+    const {getByTestId} = render(<Login/>)
+    expect(getByTestId('password-input')).toBeTruthy()
+})
+
