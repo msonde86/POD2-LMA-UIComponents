f205f1653ce48eb5ce0ee7f5d78ca1d6dd50a415
Bharateshwar Singh <bharateshwarsp@gmail.com>
2020-09-17 10:52:42 +0530

    added backend url

--- src/app-strings/backend-urls.js
+++ src/app-strings/backend-urls.js
@@ -1,2 +1,2 @@
-export const LOGIN_URL = "localhost:8081/api/login/authenticate"
-
+export const LOGIN_URL = 'http://localhost:8081/api/login/authenticate'
+export const FILLER_URL='http://localhost:8082/loan/data/filter'
